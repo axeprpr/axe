5bad1fcaf27ec7c6b1f1b1cbc8bd67a1b79f6c50
Axe.Xu <axe3@qq.com>
2020-01-15 11:30:06 +0800

    Make batch SSH and SCP run non-interactively and validate hosts

--- tests/test_axe.py
+++ tests/test_axe.py
@@ -41,6 +41,44 @@ class AxeTests(unittest.TestCase):
         self.assertEqual(exit_code, 1)
         mocked_print.assert_called_with("Failed. No such file or directory: missing-file")
 
+    def test_main_rejects_command_without_hosts(self):
+        with mock.patch("builtins.print") as mocked_print:
+            exit_code = axe.main(["-c", "hostname"])
+        self.assertEqual(exit_code, 1)
+        mocked_print.assert_called_with("Failed. No host specified for command execution.")
+
+    def test_main_rejects_scp_without_hosts(self):
+        with mock.patch("builtins.print") as mocked_print:
+            exit_code = axe.main(["-s", "demo.txt"])
+        self.assertEqual(exit_code, 1)
+        mocked_print.assert_called_with("Failed. No host specified for SCP.")
+
+    def test_ssh_command_mode_waits_for_completion(self):
+        child = mock.Mock()
+        with mock.patch.object(axe.pexpect, "spawn", return_value=child) as spawn_mock, \
+             mock.patch.object(axe, "expect_and_send_password") as expect_mock, \
+             mock.patch.object(axe, "wait_for_child") as wait_mock, \
+             mock.patch.object(axe, "interact_with_child") as interact_mock:
+            axe.ssh("root", "secret", "10.0.0.8", command="hostname")
+
+        spawn_mock.assert_called_once()
+        expect_mock.assert_called_once_with(child, "secret")
+        wait_mock.assert_called_once_with(child)
+        interact_mock.assert_not_called()
+        self.assertIs(child.logfile_read, axe.sys.stdout)
+
+    def test_scp_waits_for_completion(self):
+        child = mock.Mock()
+        with mock.patch.object(axe.pexpect, "spawn", return_value=child) as spawn_mock, \
+             mock.patch.object(axe, "expect_and_send_password") as expect_mock, \
+             mock.patch.object(axe, "wait_for_child") as wait_mock:
+            axe.scp("root", "secret", "10.0.0.8", "demo.txt", destination="/tmp")
+
+        spawn_mock.assert_called_once()
+        expect_mock.assert_called_once_with(child, "secret")
+        wait_mock.assert_called_once_with(child)
+        self.assertIs(child.logfile_read, axe.sys.stdout)
+
 
 if __name__ == "__main__":
     unittest.main()
